ea6ea88d15e3145caa562f0b613b45cc28b5afa9
Kjnaveen23 <Naveen@evoltechgroup.com>
2025-11-07 21:20:09 +0530

    fix: conflict-cleared

--- src/app/contact/Content/Section3.tsx
+++ src/app/contact/Content/Section3.tsx
@@ -36,7 +36,6 @@ const Section3 = () => {
           />
         </div>
         <div className="col-span-4 sm:col-span-8 lg:col-span-12  col-start-1 lg:col-start-3 w-full flex flex-col md:flex-row h-full justify-start gap-10 md:gap-20 items-center -mt-20 md:mt-0 pb-20">
-        <div className="col-span-4 sm:col-span-8 lg:col-span-12  col-start-1 lg:col-start-3 w-full flex flex-col md:flex-row h-full justify-start gap-10 md:gap-20 items-center -mt-20 md:mt-0 pb-20">
           <div className="w-fit flex items-center md:items-start justify-center mt-10 md:-mt-10">
             <TimeLine />
           </div>
@@ -44,7 +43,6 @@ const Section3 = () => {
             <Form />
           </div>
         </div>
-      </div>
       </div>
     </section>
   );
